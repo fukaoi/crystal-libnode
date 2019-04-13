3b56b7d544df964056249a43bfe039d48dcfa443
fukaoi <goodakira7@gmail.com>
2019-04-13 09:01:04 +0900

    WIP

--- tasks/build_node.cr
+++ tasks/build_node.cr
@@ -3,23 +3,39 @@ require "file_utils"
 
 class BuildNode < LuckyCli::Task
   summary "Build Node.js source. Need to exec before `lucky build`"
+  CUSTOM_NPM_DIR = "bin/node-#{NODE_VERSION}"
+  
+  def initialize
+    FileUtils.mkdir(EXTERNAL_DIR) unless Dir.exists?(EXTERNAL_DIR)
+  end
 
   def call
-    FileUtils.mkdir(EXTERNAL_DIR) unless Dir.exists?(EXTERNAL_DIR)
     git_clone_tag
-    FileUtils.mkdir("./bin/node-#{LIBNODE_VERSION}") unless Dir.exists?("./bin/node-#{LIBNODE_VERSION}")
-    build_nodejs("--prefix=#{ENV["PWD"]}/bin/node-#{LIBNODE_VERSION}")  # create node binary
-    system("cd #{NODEJS_SOURCE_DIR};make install")
-    FileUtils.cp("#{NODEJS_SOURCE_DIR}/node", "./bin/node-#{LIBNODE_VERSION}/bin/")
-    system("sed -e '1d' ./bin/node-#{LIBNODE_VERSION}/bin/npm > ./bin/node-#{LIBNODE_VERSION}/bin/npm") 
-    system("sed -i '1s/^/#!./node\n/' ./bin/node-#{LIBNODE_VERSION}/bin/npm")
-    system("export LD_LIBRAY_PATH")
+    # create custom node, npm binary
+    install_custom_node_npm
+    replace_path_innpm
 
+    # create libnode.a shared objectj
     # copy_patch_files
     # build_nodejs  # create libnode.a shared object
     success("Build done")
   rescue e : Exception
     failed(e.to_s)
+  end
+
+  private def install_custom_node_npm
+    FileUtils.mkdir(CUSTOM_NPM_DIR) unless Dir.exists?(CUSTOM_NPM_DIR)
+    build_nodejs("--prefix=#{ENV["PWD"]}/#{CUSTOM_NPM_DIR}")  # create node binary
+    system("cd #{NODEJS_SOURCE_DIR};make install")
+    FileUtils.cp("#{NODEJS_SOURCE_DIR}/node", "#{CUSTOM_NPM_DIR}/bin/")
+  end
+
+  private def replace_path_innpm
+    npm_path = "#{CUSTOM_NPM_DIR}/bin/npm"
+    body = File.read(npm_path).gsub("/usr/bin/env node","#{CUSTOM_NPM_DIR}/bin/node")
+    File.write(npm_path, body)
+    # dry run 
+    system("LD_LIBRAY_PATH=#{CUSTOM_NPM_DIR}/lib #{CUSTOM_NPM_DIR}/bin/node #{CUSTOM_NPM_DIR}/bin/npm")
   end
 
   private def copy_patch_files
