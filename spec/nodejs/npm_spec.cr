b62ac64e78954323d82cd176d72be59724127a02
fukaoi <goodakira7@gmail.com>
2019-04-13 18:45:59 +0900

    Fix npm path

--- spec/nodejs/npm_spec.cr
+++ spec/nodejs/npm_spec.cr
@@ -1,5 +1,5 @@
 require "../spec_helper"
 
 def npm(page_name : String) : Void
-  system("cd /tmp/;./bin/node-v10.0.0/bin/npm install #{page_name}")
+  system("cd /tmp/;#{Node::Npm.path} install #{page_name}")
 end
