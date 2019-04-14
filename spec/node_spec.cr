bca6bf08cdd4eeb7403c248fb4ad871d2e4c0f96
fukaoi <goodakira7@gmail.com>
2019-04-14 13:18:08 +0900

    added spec pattern

--- spec/node_spec.cr
+++ spec/node_spec.cr
@@ -21,6 +21,23 @@ describe node = Node::Js.new do
 end
 
 describe Node::Npm do
-  it "Evaluate js code" do
+  it "Get npm path" do
+    npm_path = Node::Npm.path
+    npm_path.empty?.should be_false
+  end
+
+  it "Get npm path with argument" do
+    npm_path = Node::Npm.path("v10.0.0")
+    npm_path.empty?.should be_false
+  end
+
+  it "Execute parse node version" do
+    node_version = Node::Npm.parse_node_version
+    node_version.empty?.should be_false
+  end
+
+  it "Execute parse node version with argument" do
+    node_version = Node::Npm.parse_node_version("v10.0.0")
+    node_version.empty?.should be_false
   end
 end
