77183b7396079642fe07e0c6085dcc6814951492
fukaoi <goodakira7@gmail.com>
2019-04-14 12:35:45 +0900

    Refactoring: moved task/test.cr to spec/test_main_spec.cr

---
+++ spec/ext/test_main_spec.cr
@@ -0,0 +1,24 @@
+require "../spec_helper"
+require "../../tasks/defined.cr"
+require "../../src/nodejs"
+
+file_name = "test_main"
+describe file_name do
+  it "Execute #{file_name}.cc" do
+    cmd = <<-CMD
+      g++ \
+      -g \
+      -I #{NODEJS_SOURCE_DIR}/src/  \
+      -I #{NODEJS_SOURCE_DIR}/deps/v8/include/ \
+      -I #{NODEJS_SOURCE_DIR}/deps/uv/include/ \
+      -I src/ext/ \
+      -std=gnu++11  \
+      spec/ext/#{file_name}.cc  \
+      -L#{LIBRARY_DIR} #{LIBRARY_DIR}/libnode.so.#{LIBNODE_VERSION}  \
+      -o /tmp/#{file_name}
+    CMD
+    system(cmd).should be_true
+    system("LD_LIBRARY_PATH=#{LIBRARY_DIR} /tmp/#{file_name}").should be_true
+   end
+end
+
