ea5e2211e2ba6af39d9c3db91de17fdcd370e666
fukaoi <goodakira7@gmail.com>
2019-04-19 13:30:09 +0900

    modified clean class

--- tasks/clean.cr
+++ tasks/clean.cr
@@ -2,23 +2,12 @@ require "./defined"
 require "file_utils"
 
 class Clean < LuckyCli::Task
-  summary "clean up bin/, libnode/ directory"
+  summary "Full clean up"
 
   def call
     FileUtils.rm_r("bin") if Dir.exists?("bin")
     FileUtils.rm_r("libnode") if Dir.exists?("libnode")
     system("shards build")
-    success("Clean up")
-  rescue e : Exception
-    failed(e.to_s)
-  end
-end
-
-class FullClean < LuckyCli::Task
-  summary "Full clean up"
-
-  def call
-    Clean.new.call
     FileUtils.rm_r("externals") if Dir.exists?("externals")
     success("Full clean up")
   rescue e : Exception
