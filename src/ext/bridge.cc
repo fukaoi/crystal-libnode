af95e6efd881d3c1ec62ca04a302f50baed470c9
fukaoi <goodakira7@gmail.com>
2019-04-19 15:42:33 +0900

    Refactoring split header and body

--- src/ext/bridge.cc
+++ src/ext/bridge.cc
@@ -1,35 +1,4 @@
-#ifndef BRIDGE_H
-#define BRIDGE_H
-#include <string.h>
-#include <string>
-#include <iostream>
-#include "node_lib.h"
-#include "node.h"
-#include "v8.h"
-
-using namespace std;
-using namespace v8;
-
-/////// header
-extern "C" {
-  void init();
-  const char* eval(const char* js_code);
-  void callback();
-  void deinit();
-}
-
-const char* toCrystalString(const String::Utf8Value &value);
-const char* toCString(const String::Utf8Value &value);
-
-typedef struct {
-  const char *crystal_type;
-  const char *js_response;
-} TupleCr;
-
-TupleCr createReponseType();
-/////// header
-
-
+#include "bridge.h"
 
 void init(){
   node::Initialize("crytal-nodejs");
@@ -68,5 +37,4 @@ TupleCr createReponseType() {
   return tuple;
 } 
 
-#endif // BRIDGE_H
 
