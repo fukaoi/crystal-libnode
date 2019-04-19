4400a46f3c93531afeb8fd51afdc3ea566e60a9c
fukaoi <goodakira7@gmail.com>
2019-04-19 15:53:44 +0900

    Refactoring C/C++ test program.

--- spec/ext/libbridge_main.c
+++ spec/ext/libbridge_main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include "node.h"
 #include "node_lib.h"
+#include "bridge.h"
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
   init();
-  eval("const a = 1; a *= 200;console.log(a)");
+  eval("const a = 'c test program';console.log(a);");
   callback();
   deinit();
   return 0;
