fba2d13c0250d0edf4707d940082b40a2be5bbd1
fukaoi <goodakira7@gmail.com>
2019-04-23 12:37:33 +0900

    init

---
+++ src/node/non_local_exit.cr
@@ -0,0 +1,20 @@
+module Node::NonLocalExit
+  def return(result : String)
+    "throw new Error(JSON.parse(JSON.stringify(return: {#{result}})))"
+  end
+
+  def surround(source : String)
+    <<-CMD
+    "try {
+      #{source}
+    } catch(tag) {
+      if (tag.return == undefined) {
+        console.log('try catch: Exception');
+        return tag;
+      }     
+      console.log('try catch: result');
+      return tag.result;
+    }"
+    CMD
+  end
+end
